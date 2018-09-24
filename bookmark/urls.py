a21e0e5e51c197e60b42de2c89ae3806affdbbcb
EunHyeJung <ehye826@gmail.com>
2018-09-24 13:42:04 +0900

    북마크 수정,삭제  url, view코드 수정, 템플릿(html) 추가

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -7,5 +7,8 @@ app_name = 'bookmark'
 urlpatterns = [
     path('', BookmarkLV.as_view(), name='index'),
     path('<int:pk>/', BookmarkDV.as_view(), name='detail'),
-    path('add/', BookMarkCreateView.as_view(), name='add')
+    path('add/', BookMarkCreateView.as_view(), name='add'),
+    path('change/', BookmarkChangeLV.as_view(), name='change'),
+    path('<int:pk>/update/', BookmarkUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', BookmarkDeleteView.as_view(), name='delete'),
 ]
--- bookmark/views.py
+++ bookmark/views.py
@@ -5,6 +5,8 @@ from bookmark.models import Bookmark
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from mysite.views import LoginRequiredMixin
+
+
 # ListView
 class BookmarkLV(ListView):
     model = Bookmark
@@ -23,3 +25,21 @@ class BookMarkCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super(BookMarkCreateView, self).form_valid(form)
+
+
+class BookmarkChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'bookmark/bookmark_change_list.html'
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(owner=self.request.user)
+
+
+class BookmarkUpdateView(LoginRequiredMixin, UpdateView):
+    model = Bookmark
+    fields = ['title', 'url']
+    success_url = reverse_lazy('bookmark:index')
+
+
+class BookmarkDeleteView(LoginRequiredMixin, DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
